0de80473d1d8b37e4ca0d4ca919e3117a6954264
Murtaza Naiyar <murtazanaiyar@gmail.com>
2021-08-30 22:15:58 +0530

    Code Cleanup

--- Projects/Banking-Project/connection.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'user'
-
-import cx_Oracle
-import os
-#you must have Oracle database userId and password for connection from database.
-
-con = cx_Oracle.connect('hr/manager@xe')
-cur = con.cursor()
